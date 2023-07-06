a287e64a71287c6a376ad54ddc66857e31b98e77
Owner <albertolukehartono@gmail.com>
2023-07-06 21:43:26 +0700

    final

--- HTML Files/Java Files/Form.js
+++ HTML Files/Java Files/Form.js
@@ -39,4 +39,4 @@ function saveFormData(event) {
 
     localStorage.setItem("formData", jsonData);
 
-    window.location.href = "Additional HTML/thank-you.html";}
+    window.location.href = "../Additional HTML/thank-you.html";}
